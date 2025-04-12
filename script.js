f50dd71612562cf2411ac7d529021d2e6c7b9939
gianmromero <gianmromerocastro@gmail.com>
2025-04-12 08:14:08 -0500

    Checkmarks working

--- script.js
+++
@@ -1,6 +0,0 @@
-const slider = document.querySelector('.generator__form-slider');
-
-slider.addEventListener('input', function() {
-    const value = (this.value - this.min) / (this.max - this.min) * 100;
-    this.style.background = `linear-gradient(to right, var(--lime-green) ${value}%, var(--very-dark-grey) ${value}%)`;
-});
